334d0147a7266d917d083ec46e0024448a1f4983
LucreciaUA <118127063+LucreciaUA@users.noreply.github.com>
2024-02-27 03:53:50 +0200

    1

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,80 @@
+import { createContactSchema, updateContactSchema } from "../schemas/contactsSchemas.js";
 import contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async(req, res) => {
+    try {
+        const contacts = await contactsService.listContacts();
+        
+        res.status(200).json(contacts);
+    } catch (error) {
+        console.error('Error fetching contacts:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+export const getOneContact = async(req, res) => {
+    try {
+        const { id } = req.params;
+        
+        const contact = await contactsService.getContactById(id);
+        
+        res.status(200).json(contact);
+    } catch (error) {
+        console.error('Error fetching contact:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
 
-export const getOneContact = (req, res) => {};
+export const deleteContact = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const contact = await contactsService.removeContact(id);
+        
+        res.status(200).json(contact);
+    } catch (error) {
+        console.error('Error deleting contact:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
 
-export const deleteContact = (req, res) => {};
+export const createContact = async(req, res) => {
+    try {
+        const { name, email, phone } = req.body;
+        const validationResult = createContactSchema.validate({
+            name: name,
+            email: email,
+            phone: phone
+        });
+        console.log(req.body)
+      if (validationResult.error) {
+       return res.status(400).json({ message: validationResult.error.message });
+      }
 
-export const createContact = (req, res) => {};
+        const contacts = await contactsService.addContact(name, email, phone );
+        
+        res.status(200).json(contacts);
+    } catch (error) {
+        console.error('Error adding contact:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, email, phone } = req.body;
+        const validationResult = updateContactSchema.validate({
+            name: name,
+            email: email,
+            phone: phone});
+        if (validationResult.error) {
+            return res.status(400).json({ message: validationResult.error.message });
+        }
+    const contacts = await contactsService.updateContact(name, email, phone, id);
+        
+        res.status(200).json(contacts);
+    } catch (error) {
+        console.error('Error deleting contact:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
